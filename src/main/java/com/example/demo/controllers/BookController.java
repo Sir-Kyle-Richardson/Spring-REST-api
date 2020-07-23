4a1c7c46c655f27d541b98d867561edafe86f2ad
Kyle Richardson <richardson.a.kyle.95@gmail.com>
2020-07-23 06:52:30 -0400

    Book delete update endpoint

--- src/main/java/com/example/demo/controllers/BookController.java
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controllers;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.UUID;
 
 import com.example.demo.helpers.JsonLoader;
+import com.example.demo.models.Book;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,18 +25,42 @@ public class BookController {
     }
     
     @PostMapping("/books")
-    public String createBook() {
+    public String createBook(@RequestBody Book newBook) throws IOException {
+        List<Book> books = JsonLoader.loadBooks();
+        books.add(newBook);
+        JsonLoader.saveBooks(books);
         return String.format("Successfully created book");
     }
 
-    @DeleteMapping("/books/:id")
-    public String deleteBook() {
-     
-        return String.format("Successfully deleted book");
+    @DeleteMapping("/books/{id}")
+    public String deleteBook(@PathVariable UUID id) throws IOException {
+        List<Book> books = JsonLoader.loadBooks();
+        ListIterator<Book> booksIterator = books.listIterator();
+        while (booksIterator.hasNext()) {
+            Book book = booksIterator.next();
+            if (book.getId().equals(id)) {
+                booksIterator.remove();
+                JsonLoader.saveBooks(books);
+                return String.format("Successfully deleted book");
+            }
+        }
+
+        return String.format("Failed to delete book");
     }
 
-    @PutMapping("/books/:id")
-    public String updateBook() {
-        return String.format("Successfully updated book");
+    @PutMapping("/books/{id}")
+    public String updateBook(@PathVariable UUID id, @RequestBody Book updatedBook) throws IOException {
+        List<Book> books = JsonLoader.loadBooks();
+        ListIterator<Book> booksIterator = books.listIterator();
+        while (booksIterator.hasNext()) {
+            Book book = booksIterator.next();
+            if (book.getId().equals(id)) {
+                booksIterator.set(updatedBook);
+                JsonLoader.saveBooks(books);
+                return String.format("Successfully updated book");
+            }
+        }
+
+        return String.format("Failed to updated book");
     }
 }
--- src/main/java/com/example/demo/helpers/JsonLoader.java
+++ src/main/java/com/example/demo/helpers/JsonLoader.java
@@ -1,11 +1,8 @@
 package com.example.demo.helpers;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
